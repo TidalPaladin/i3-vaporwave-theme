2e3b8b57acfca09447803ba30818f1f493ef15bd
Chase Waggener <tidalpaladin@gmail.com>
2018-10-16 14:35:27 -0500

    init global repo

--- data/xorg.conf.d/symlinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Generates symlinks from config files in the repo
-# to various the appropriate system locations
-
-# Symlinks to /etc/X11/xorg.conf.d/*
-# Requires sudo
-echo "Need password to edit /etc/X11/xorg.conf.d"
-ls -A1 | grep -Ev 'symlinks.sh' | sudo parallel --tty ln -vLsiT $PWD/{} /etc/X11/xorg.conf.d/{}
-
