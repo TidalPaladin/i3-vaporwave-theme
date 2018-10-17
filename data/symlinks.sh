743c4fc5f4bc423a5df9146ac287543e74e94252
Chase Waggener <tidalpaladin@gmail.com>
2018-10-16 20:08:04 -0500

    fix

--- data/symlinks.sh
+++ data/symlinks.sh
@@ -3,5 +3,5 @@
 # to various the appropriate system locations
 
 echo "Need password to edit /etc/fstab"
-sudo cp /etc/fstab /etc/fstab.bak && sudo ln -viLsT $PWD/fstab /etc/fstab
+sudo cp /etc/fstab /etc/fstab.bak && sudo cp fstab /etc/fstab
 
